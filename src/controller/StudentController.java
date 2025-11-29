7f47959bfd5e588c631cccf38c8a5ed3b458bc1e
Samhitha Girish Jois <samhitha.girishjois@gmail.com>
2025-11-29 15:42:13 +0100

    controller, added annotations and first baby steps

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -1,5 +1,15 @@
 package controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class StudentController {
-}
+        @RequestMapping("/hello")
+        @ResponseBody
+        public String helloGFG() {
+            return "Hello GeeksForGeeks";
+        }
+    }
