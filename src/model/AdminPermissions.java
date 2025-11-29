cd84a15ffc233cda088b8ef63f09e276e42debea
Samhitha Girish Jois <samhitha.girishjois@gmail.com>
2025-11-29 15:27:10 +0100

    model part of MVC

---
+++ src/model/AdminPermissions.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum AdminPermissions {
+    DE,MU,ER,IN,EU,NONE;
+}
--- src/model/Administrator.java
+++ src/model/Administrator.java
@@ -1,7 +1,22 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Administrator extends Person{
+
+
+    public List<AdminPermissions> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<AdminPermissions> permissions) {
+        this.permissions = permissions;
+    }
+
+    private List<AdminPermissions> permissions;
     public Administrator(String personID, String firstName, String lastName) {
         super(personID, firstName, lastName);
+        permissions = new ArrayList<>();
     }
 }
