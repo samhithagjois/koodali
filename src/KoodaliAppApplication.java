7548dcd4fed553712f9ce08a00056fe315ae4028
Samhitha Girish Jois <samhitha.girishjois@gmail.com>
2025-12-03 13:14:11 +0100

    KoodaliAppApplication

--- src/KoodaliAppApplication.java
+++ src/KoodaliAppApplication.java
@@ -1,4 +1,15 @@
+import org.springframework.boot.SpringApplication;
+
 public class KoodaliAppApplication {
+
+    KoodaliAppApplication(){
+
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(KoodaliAppApplication.class, args);
+    }
 
     /*@SpringBootApplication
 public class RestServiceApplication {
