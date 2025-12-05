004364f733c1a99ad56cfb9623b8465321b39f1c
Samhitha Girish Jois <samhitha.girishjois@gmail.com>
2025-12-05 14:32:36 +0100

    AdminOperationService reassignStudent, addTeacherToSection, addStudentToSection, remove TEacher/Student from System

--- src/service/AdminOperationService.java
+++ src/service/AdminOperationService.java
@@ -1,11 +1,10 @@
 package service;
 
+import model.*;
 import repository.AdminRepository;
 import repository.SectionRepository;
 import repository.StudentRepository;
 import repository.TeacherRepository;
-
-import model.*;
 import service.exceptions.*;
 
 public class AdminOperationService {
@@ -17,7 +16,7 @@ public class AdminOperationService {
 
     private final SectionRepository sectionRepo = new SectionRepository();
 
-    public AdminOperationService(){
+    public AdminOperationService() {
 
     }
 
@@ -26,103 +25,157 @@ public class AdminOperationService {
      * So the admin permission list is two  letters and corresponds with the first two letters of the section
      * we compare these two and if its not equal we throw a IllegalAdminActionException.
      * This is an internal method. I don't think this should be visible outside
-     * @param admin administrator
-     * @param section Section to check
-     * @throws IllegalAdminActionException if for example a Munich admin tries changing Ingolstadt
-     * @return true or false
      *
-     * */
+     * @param admin   administrator
+     * @param section Section to check
+     * @return true or false
+     * @throws IllegalAdminActionException if for example a Munich admin tries changing Ingolstadt
+     */
     public boolean checkPermissionToModify(Administrator admin, Section section) throws IllegalAdminActionException {
-        String area = section.getName().toString().substring(0,1);
-        for (AdminPermissions permission:admin.getPermissions()  ) {
-            if(!permission.toString().equals(area)){
+        String area = section.getName().toString().substring(0, 1);
+        for (AdminPermissions permission : admin.getPermissions()) {
+            if (!permission.toString().equals(area)) {
                 throw new IllegalAdminActionException();
             }
         }
         return true;
     }
 
-    public Student addStudentToSection (String adminID, String studentID, String sectionID) throws
+    /**
+     * helper methods to reduce code duplication! (I sound super fancy now don't I)
+     * returns the Person/Section it finds, or throws the respective Exception
+     */
+    private Administrator findAdmin(String adminID) throws AdminNotFoundException {
+        Person admin = adminRepo
+                .findByID(adminID)
+                .orElseThrow(AdminNotFoundException::new);
+        return (Administrator) admin;
+    }
+
+    private Teacher findTeacher(String teacherID) throws TeacherNotFoundException {
+        Person teacher = teacherRepo
+                .findByID(teacherID)
+                .orElseThrow(TeacherNotFoundException::new);
+        return (Teacher) teacher;
+    }
+
+    private Student findStudent(String studentID) throws StudentNotFoundException {
+        Person student = studentRepo
+                .findByID(studentID)
+                .orElseThrow(StudentNotFoundException::new);
+        return (Student) student;
+    }
+
+    private Section findSection(String sectionName) throws SectionNotFoundException {
+        return sectionRepo
+                .findSectionByName(sectionName)
+                .orElseThrow(SectionNotFoundException::new);
+    }
+
+
+    public Student addStudentToSection(String adminID, String studentID, String sectionID)
+            throws
             StudentNotFoundException,
             SectionNotFoundException,
             AdminNotFoundException,
             IllegalAdminActionException {
 
-        Person admin = adminRepo
-                .findByID(adminID)
-                .orElseThrow(AdminNotFoundException::new);
-        Person student = studentRepo
-              .findByID(studentID)
-              .orElseThrow(StudentNotFoundException::new);
+        Student student = findStudent(studentID);
+        Administrator admin = findAdmin(adminID);
 
-      Section section = sectionRepo
-              .findSectionByName(sectionID)
-              .orElseThrow(SectionNotFoundException::new);
+        Section section = findSection(sectionID);
 
-      if(student instanceof Student s && admin instanceof Administrator a){
-          if(checkPermissionToModify(a, section)){
-             throw new IllegalAdminActionException();
-          }
-          s.setSection(section.getName());
-          section.getStudents().put(s.getID(),s);
-          studentRepo.update(s);
-          sectionRepo.updateSection(section);
+        if (checkPermissionToModify(admin, section)) {
+            throw new IllegalAdminActionException();
+        }
+        student.setSection(section.getName());
+        section.getStudents().put(student.getID(), student);
+        sectionRepo.updateSection(section);
+        return studentRepo.update(student);
 
-      }else{
-          throw new RuntimeException("imposter go brrr");
-      }
-      return s;
+
     }
 
 
-    public Teacher addTeacherToSection (String adminID, String teacherID, String sectionID) throws
-             SectionNotFoundException,
+    public Teacher addTeacherToSection(String adminID, String teacherID, String sectionID) throws
+            SectionNotFoundException,
             AdminNotFoundException,
             IllegalAdminActionException,
             TeacherNotFoundException {
 
-        Person admin = adminRepo
-                .findByID(adminID)
-                .orElseThrow(AdminNotFoundException::new);
-        Person teacher = teacherRepo
-                .findByID(teacherID)
-                .orElseThrow(TeacherNotFoundException::new);
+        Administrator admin = findAdmin(adminID);
 
-        Section section = sectionRepo
-                .findSectionByName(sectionID)
-                .orElseThrow(SectionNotFoundException::new);
+        Section section = findSection(sectionID);
 
-        if(teacher instanceof Teacher t && admin instanceof Administrator a){
-            if(checkPermissionToModify(a, section)){
+        Teacher teacher = findTeacher(teacherID);
+
+        if (checkPermissionToModify(admin, section)) {
+            throw new IllegalAdminActionException();
+        }
+        teacher.setSection(section);
+        section.getTeachers().put(teacher.getID(), teacher);
+        sectionRepo.updateSection(section);
+        return teacherRepo.update(teacher);
+
+
+    }
+
+
+    public Student reassignStudentToSection(String adminID, String studentID, String sectionID) throws IllegalAdminActionException {
+        try {
+            Student student = findStudent(studentID);
+            Administrator admin = findAdmin(adminID);
+            Section section = findSection(sectionID);
+
+            if (checkPermissionToModify(admin, section)) {
                 throw new IllegalAdminActionException();
             }
-            t.setSection(section);
-            section.getTeachers().put(t.getID(),t);
-            teacherRepo.update(t);
+            //remove student from old section
+            sectionRepo
+                    .findSectionByName(student.getSection().name())
+                    .ifPresent(section1 -> section1.getStudents().remove(studentID, student));
+            //change the section in the student
+            student.setSection(section.getName());
+            //add student to new class
+            section.getStudents().put(studentID, student);
+            //update section in sectionRepo
             sectionRepo.updateSection(section);
-
-        }else{
-            throw new RuntimeException("imposter go brrr");
+            //update student in studentRepo and return
+            return studentRepo.update(student);
+        } catch (StudentNotFoundException | AdminNotFoundException | SectionNotFoundException se) {
+            throw new RuntimeException(se);
         }
-        return t;
     }
 
 
-    public Student reassignStudentToSection(String adminID, String studentID,String sectionID){
-        //TODO!
-        //TODO : delete student from section. delete section from student. add student to new section. add section to student
-        // save the respective students/sections.
-        return null;
+    public Student removeStudentFromSystem(String adminID, String studentID) {
+        try {
+            findAdmin(adminID);
+        } catch (AdminNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        return studentRepo.delete(studentID);
     }
 
 
+    public Teacher removeTeacherFromSystem(String adminID, String teacherID) {
 
-    //reassignStudentToSection(Student student, Section section)
+        try {
+            findAdmin(adminID);
+        } catch (AdminNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return teacherRepo.delete(teacherID);
+    }
+
+
     //deleteStudentFromSection(Student student, Section section)
-    //removeStudentFromSystem(Student student)
+
 
     //addCoTeacherToSection(Teacher teacher, Section section)
     //deleteTeacherFromSection(Teacher teacher, Section section)
-    //removeTeacherFromSystem(Teacher teacher)
 
 }
