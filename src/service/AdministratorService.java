ad88aed71ac434766c170a7605b316b2d3bc9141
Samhitha Girish Jois <samhitha.girishjois@gmail.com>
2025-12-07 23:39:27 +0100

    Wow. wanted to implement a password system and now I have like 10 new classes and methods. Plus I  guess I'll have to learn Angular from scratch now. I NEED TO SEE THINGS UHHHH

--- src/service/AdministratorService.java
+++ src/service/AdministratorService.java
@@ -1,7 +1,20 @@
 package service;
 
+import model.Administrator;
+import repository.AdminRepository;
+
 public class AdministratorService {
     //createAdmin
     //checkCredentials
     //removeAdmin
+
+    private final AdminRepository adminRepo = new AdminRepository();
+
+    public AdministratorService(){
+
+    }
+
+    public Administrator removeAdmin(String adminID){
+       return adminRepo.getAdmins().remove(adminID);
+    }
 }
