a9962fabcdfa6a1f817239cc18550d6e992524e0
Samhitha Girish Jois <samhitha.girishjois@gmail.com>
2025-12-08 23:00:26 +0100

    deleted all the login stuff. No need for that yay

--- src/service/exceptions/WrongPasswordException.java
+++
@@ -1,9 +0,0 @@
-package service.exceptions;
-
-public class WrongPasswordException extends Exception{
-
-
-    public WrongPasswordException() {
-        super("Wrong password. Try again");
-    }
-}
