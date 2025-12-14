4043205fdf113933c654fb634cce5d4469293932
Samhitha Girish Jois <samhitha.girishjois@gmail.com>
2025-12-14 17:26:22 +0100

    leader board

--- src/repository/LeaderboardRepository.java
+++ src/repository/LeaderboardRepository.java
@@ -1,13 +1,22 @@
 package repository;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class LeaderboardRepository {
+
+   public HashMap<String, Integer> getPointsMap() {
+      return pointsMap;
+   }
 
    private final HashMap<String,Integer> pointsMap;
 
    public LeaderboardRepository() {
       this.pointsMap = new HashMap<>();
+   }
+
+   public List<String> getSorted(){
+      return pointsMap.keySet().stream().sorted().toList();
    }
 
 
--- src/service/LeaderboardService.java
+++ src/service/LeaderboardService.java
@@ -1,4 +1,13 @@
 package service;
 
+import repository.*;
+
 public class LeaderboardService {
+
+    private final LeaderboardRepository leaderboardRepository = new LeaderboardRepository();
+
+    public LeaderboardService(){
+
+    }
+
 }
