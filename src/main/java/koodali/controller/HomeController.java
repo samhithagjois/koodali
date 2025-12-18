0ba9702a50a056bf026aeedf77bc3d99d58e984e
Samhitha Girish Jois <samhitha.girishjois@gmail.com>
2025-12-18 23:11:40 +0100

    home controller

---
+++ src/main/java/koodali/controller/HomeController.java
@@ -0,0 +1,13 @@
+package koodali.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
+public class HomeController {
+    //Login to Admin view
+    //login to teachers view
+    //for students
+
+}
