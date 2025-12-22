acaf550e9f994bd88ae257b598bdd0094ce0eb64
Samhitha Girish Jois <samhitha.girishjois@gmail.com>
2025-12-22 21:47:31 +0100

    shifted the tests over

---
+++ src/test/java/ServiceTest.java
@@ -0,0 +1,12 @@
+import koodali.model.*;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ServiceTest {
+
+    @Test
+    public void haha(){
+           }
+}
