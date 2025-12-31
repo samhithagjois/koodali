2e843bb4737b8121866453e28bd41659ea1adfab
Samhitha Girish Jois <samhitha.girishjois@gmail.com>
2025-12-31 21:55:10 +0100

    homework points table services todo methods

--- src/main/java/koodali/service/HomeworkPointsExcelService.java
+++ src/main/java/koodali/service/HomeworkPointsExcelService.java
@@ -3,7 +3,7 @@ package koodali.service;
 public class HomeworkPointsExcelService {
     //TODO: methods
     // 1. createTemplate
-    // 2. createTemplateForTeacher
+    // 2. createTemplateForTeacher(Teacher teacher, List<Section> sections)
     // 3. validateUploadedFile
     // 4. parseFile....
 }
--- src/main/java/koodali/service/HomeworkPointsService.java
+++ src/main/java/koodali/service/HomeworkPointsService.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Service;
 public class HomeworkPointsService {
 
     //TODO
-    //
+    //HomeworkPointsService
     // : methods :
     // 1. getListForSection(String sectionID)
     // 2. updateCell(HomeworkPointsEntity entity)
     // 3. getListForAllSectionsOfTeacher(String teacherID)
-    // 4. clearCell(int
+    // 4. clearCell(HomeworkPointsEntity entity)
     // 5. ...
 
 
