e0bcb757c006ad9170eed813d5cc941d99890e36
Samhitha Girish Jois <samhitha.girishjois@gmail.com>
2026-01-06 16:29:34 +0100

    SectionTeacherOverview

--- koodali-frontend/src/app/features/admin/section-overview/section-teacher-overview/section-teacher-overview.ts
+++ koodali-frontend/src/app/features/admin/section-overview/section-teacher-overview/section-teacher-overview.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, RouterLink} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-section-teacher-overview',
@@ -10,6 +11,26 @@ import {RouterLink} from '@angular/router';
   templateUrl: './section-teacher-overview.html',
   styleUrl: './section-teacher-overview.css',
 })
-export class SectionTeacherOverview {
+export class SectionTeacherOverview implements OnInit{
+  classId!:string;
+  teachers : any[] = [];
+  constructor(private http: HttpClient,
+              private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.classId = this.route.snapshot.paramMap.get('id')!;
+    this.loadTeachers();
+
+  }
+
+  loadTeachers(){
+    if(this.classId == null){
+      return;
+    }
+    this.http.get<any[]>(`http://localhost:8080/api/admin/sections/${this.classId}/teachers`)
+      .subscribe(data => {
+        this.teachers = data;
+      });
+  }
 
 }
+
